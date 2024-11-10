04b557772338bf47cbe95ce2152d5ed3f764e1d6
sushma <sushma@DESKTOP-MVB8S5H>
2024-11-10 18:15:34 +0530

    task Dev commit

--- GITProj01/src/test/java/com/nit/main/AppTest.java
+++ GITProj01/src/test/java/com/nit/main/AppTest.java
@@ -1,19 +1,38 @@
 package com.nit.main;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
+
 public class AppTest {
 
-    /**
-     * Rigorous Test :-)
-     */
+    
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void testWithPositives() {
+        App app=new App();
+       int x=10;
+        int y=20;
+        int actual=30;
+        int expected=app.sum(x, y);
+        assertEquals(actual,expected);
+    }
+    @Test
+    public void testWithNegatives() {
+        App app=new App();
+       int x=-10;
+        int y=-20;
+        int actual=-30;
+        int expected=app.sum(x, y);
+        assertEquals(actual,expected);
+    }
+    @Test
+    public void testWithMixedValues() {
+        App app=new App();
+       int x=-10;
+        int y=20;
+        int actual=10;
+        int expected=app.sum(x, y);
+        assertEquals(actual,expected);
     }
 }
